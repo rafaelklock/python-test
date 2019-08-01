fa30b79de11ceabba3d9f70a6cc2e20292de76e2
Rafael Klock <kklockk@gmail.com>
2019-07-31 22:14:00 -0300

    Adicionado recursos de soma, subtração, div, mult, e divisores

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,31 @@
+# CALCULADORA Python
+# Rafael Klock
+def soma(a,b):          print(f"\n --> resultado da soma: {float(a) + float(b)}\n")
+def subtracao(a,b):     print(f"resultado da sub: {a - b}")
+def divisao(a,b):       print(f"resultado da div: {a / b}")
+def multiplicacao(a,b): print(f"resultado da mult: {a * b}")
+
+def divisor(a,b):
+    if (int(a) % int(b)) == 0 :
+        print(f"\n --> {b} é divisor de {a}")
+    else: 
+        print(f"\n --> {b} não é divisor de {a}")
 
 
-def soma(a,b):
-    return a + b;
+valor_input = input('\nBem vindo a calculadora!\n\nInforme sua equaçao: ')
+
+if '+' in valor_input:
+    valores = valor_input.split('+')
+    operacao = '+'
+    valor_a = valores[0]
+    valor_b = valores[1]
+    soma(valor_a,valor_b)
+
+if '%' in valor_input:
+    valores = valor_input.split('%')
+    operacao = '%'
+    valor_a = valores[0]
+    valor_b = valores[1]
+    divisor(valor_a,valor_b)
+
+
