db132b2f228c51675771c6b930b5b58416328495
Rafael Klock <kklockk@gmail.com>
2019-08-08 01:30:36 -0300

    update

---
+++ classes_python/model.py
@@ -0,0 +1,50 @@
+class Relationship:
+    def __init__(self, name, _from, to, on):
+        self._name = name
+        self._from = _from
+        self._to = to
+        self._on = on
+
+
+class DataTable:
+    def __init__(self, name):
+        self._name = name
+        self._columns = []
+        self._references = []
+        self._referenced = []
+        self._data = []
+
+    def add_column(self, name, kind, description=""):
+        column = Column(name, kind, description=description)
+        self._columns.append(column)
+        return column
+
+    def add_references(self, name, to, on):
+        relationship = Relationship(name, self, to, on)
+        self._references.append(relationship)
+
+    def add_referenced(self, name, by, on):
+        relationship = Relationship(name, by, self, on)
+        self._referenced.append(relationship)
+
+
+class Column:
+    def __init__(self, name, kind, description=""):
+        self._name = name
+        self._kind = kind
+        self._description = description
+
+
+class Retangulo:
+    def __init__(self, altura, largura):
+        self.largura = largura
+        self.altura = altura
+
+    def calc_area(self):
+        return self.altura * self.largura
+
+
+if __name__ == '__main__':
+    retangulo1 = Retangulo(10, 20)
+    area = retangulo1.calc_area()
+    print(area)
